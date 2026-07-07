7f1ec8470dec928425369213426c4a12677ce5ac
deniz <denizkahramanoglu@gmail.com>
2026-07-07 15:08:34 +0300

    Testing LiquidBase adding db

---
+++ src/test/java/com/example/LiquiBaseTest/LiquiBaseTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LiquiBaseTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquiBaseTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
